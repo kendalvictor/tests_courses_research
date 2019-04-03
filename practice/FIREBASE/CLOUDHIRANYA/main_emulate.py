662690730f3469e765799fd4b223ce26ae917fdb
Victor Villacorta <vvillacorta@rmgperustaff.com>
2019-04-03 10:19:58 -0500

    UP functions firebase python OK

---
+++ practice/FIREBASE/CLOUDHIRANYA/main_emulate.py
@@ -0,0 +1,17 @@
+# File: main_emulate.py
+import flask
+from main import heroes
+
+app = flask.Flask('functions')
+methods = ['GET', 'POST', 'PUT', 'DELETE']
+
+
+@app.route('/heroes', methods=methods)
+@app.route('/heroes/<path>', methods=methods)
+def catch_all(path=''):
+    flask.request.path = '/' + path
+    return heroes(flask.request)
+
+
+if __name__ == '__main__':
+    app.run()
